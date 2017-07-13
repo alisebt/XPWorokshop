332b44812ea712d1698d3716f0ba8ea70c1c0155
alisebt@gmail.com <alisebt@gmail.com>
2017-07-13 17:08:57 +0430

    Add project files.

---
+++ MelliCalc/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MelliCalc.Startup))]
+namespace MelliCalc
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
